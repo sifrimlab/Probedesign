268991419b90b1fabe357c26477f3b7a608d0522
Ceyhun-Alar <cey_cey_ceyhun@hotmail.com>
2022-09-20 15:36:18 +0200

    Cleaning up files

--- src/parse_transcripts.py
+++ src/parse_transcripts.py
@@ -6,16 +6,12 @@ gencode_file = sys.argv[2]
 transcripts = open(transcript_file,'r').readlines()
 transcripts = [x.rstrip() for x in transcripts]
 faidx_input = [t for t in transcripts]
-<<<<<<< HEAD
 
 if "." in faidx_input[0]:
     genes = Fasta(gencode_file,split_char="|",duplicate_action='first',)
 else:
     genes = Fasta(gencode_file,split_char="|",duplicate_action='first',key_function = lambda x: x.split('.')[0])
 
-=======
-genes = Fasta(gencode_file,split_char="|",duplicate_action='first')
->>>>>>> 97124bca6d7c9bc2e5331b7c54c88d48f16f2b41
 
 with open("faidx_input.txt", "w") as textfile:
     for element in faidx_input:
@@ -23,7 +19,6 @@ with open("faidx_input.txt", "w") as textfile:
 
 with open("probes_for_alligning.fasta", "w") as textfile:
     for transcript in (faidx_input):
-<<<<<<< HEAD
         try:
             textfile.write(f'>{genes[transcript].name}\n{genes[transcript]}\n')
         except:
@@ -35,10 +30,3 @@ with open("complement_initial_probes.fasta", "w") as textfile:
             textfile.write(f'>{genes[transcript].name}\n{genes[transcript][:].complement}\n')
         except:
             pass
-=======
-        textfile.write(f'>{genes[transcript].name}\n{genes[transcript]}\n')
-
-with open("complement_initial_probes.fasta", "w") as textfile:
-    for transcript in (faidx_input):
-        textfile.write(f'>{genes[transcript].name}\n{genes[transcript][:].complement}\n')
->>>>>>> 97124bca6d7c9bc2e5331b7c54c88d48f16f2b41
